8c740efaf45c31b3553b04065e70c56f65b3b328
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-15 01:33:59 +0000

    add eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  console.log (actual, expected);
+  if (actual === expected) {
+    console.log(`😃Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`😭Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false; 
+  } else {
+    for (let i = 0 ; i < a.length; i ++ ){
+      if (a[i] !== b[i]){
+        return false; 
+        break; 
+      } else {
+        if (i === a.length-1) {
+          return true
+        } else {
+          continue;
+        }
+      }
+    }
+  }
+}
+
+
+  // eqArrays([1, 2, 3], [1, 2, 3]) // => true
+  // eqArrays([1, 2, 3], [3, 2, 1]) // => false
+  
+  // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+  // eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+  // eqArrays([1, 2, 3], [1, 2, 3])
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
