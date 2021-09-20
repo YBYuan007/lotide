c0fbf91a695774490aeb178b9cd4c321f0d56d48
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-20 22:11:59 +0000

    refactor

--- assertEqual.js
+++ assertEqual.js
@@ -11,17 +11,20 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`😃Assertion Passed: [${actual}] === [${expected}]`);
+    // console.log(`😃Assertion Passed: [${actual}] === [${expected}]`);
+    return `😃Assertion Passed: [${actual}] === [${expected}]` ; 
   } else {
-    console.log(`😭Assertion Failed: [${actual}] !== [${expected}]`);
+    // console.log(`😭Assertion Failed: [${actual}] !== [${expected}]`);
+    return `😭Assertion Failed: [${actual}] !== [${expected}]`; 
   }
 };
 
 
 
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
+module.exports=assertEqual;
 
