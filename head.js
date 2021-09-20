b617b71827668bd307e3e0429481eb10cee984ce
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-20 21:31:21 +0000

    refactor

--- head.js
+++ head.js
@@ -9,25 +9,30 @@
 
 
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`😭Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`😃Assertion Passed: [${actual}] === [${expected}]`);
+//   } else {
+//     console.log(`😭Assertion Failed: [${actual}] !== [${expected}]`);
+//   }
+// };
 
 // TEST CODE
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
+// const assertEqual = require("./assertEqual.js");
 
 const head = function(ar){
-  return ar[0]
+  return ar[0];
 }
 
+// assertEqual(head([ar]), 5);
+// assertEqual(head([ar]), "Hello");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports=head;
 
 
 
